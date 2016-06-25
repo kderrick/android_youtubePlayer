78db62bddae21c77d225a9837ebd6fd72af757fc
Kyle <kderrick1988@gmail.com>
2016-06-25 09:36:54 -0700

    update activity_youtube

--- app/src/main/java/com/epicodus/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/epicodus/youtubeplayer/YoutubeActivity.java
@@ -7,6 +7,10 @@ import com.google.android.youtube.player.YouTubePlayer;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+    private String GOOGLE_API_KEY = "TBA";
+    private String YOUTUBE_VIDEO_KEY = "TBA";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
